6968c042ddb6379e43bd11ca7ad47ab085a76516
hantsy <hantsy@gmail.com>
2018-01-28 13:46:54 +0800

    complete the IntegrationTests

--- post-service/src/test/java/com/hantsylabs/sample/springmicroservice/post/IntegrationTests.java
+++ post-service/src/test/java/com/hantsylabs/sample/springmicroservice/post/IntegrationTests.java
@@ -6,18 +6,29 @@
 package com.hantsylabs.sample.springmicroservice.post;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
 
@@ -26,6 +37,7 @@ import static org.hamcrest.CoreMatchers.is;
  */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
+@Import(IntegrationTests.TestSecurityConfig.class)
 @Slf4j
 public class IntegrationTests {
 
@@ -35,25 +47,39 @@ public class IntegrationTests {
     @Inject
     PostRepository posts;
 
+    @Inject
+    CommentRepository comments;
+
     String test_title = "test title";
     String test_content = "test content";
+    String test_comment = "test_comment";
     String slug = "";
 
     @Before
     public void setup() {
         RestAssured.port = this.port;
+        this.comments.deleteAllInBatch();
         this.posts.deleteAllInBatch();
+
         Post post = posts.save(
             Post.builder()
                 .title(test_title)
                 .content(test_content)
                 .build()
         );
-        log.debug("saving post:" + post);
+        log.debug("saved post:" + post);
         this.slug = post.getSlug();
 
         log.debug("print all posts:");
         posts.findAll().forEach(System.out::println);
+
+        Comment comment = this.comments.save(
+            Comment.builder()
+                .content(test_comment)
+                .post(new Slug(this.slug))
+                .build()
+        );
+        log.debug("saved comment:" + comment);
     }
 
     @Test
@@ -90,5 +116,194 @@ public class IntegrationTests {
         //@formatter:on
     }
 
+    @Test
+    public void testGetCommentsOfPostBySlug_shouldBeOK() throws Exception {
+        //@formatter:off
+        when()
+            .get("/posts/"+ this.slug+"/comments")
+        .then()
+            .body("content[0].content", is(test_comment))
+            .statusCode(HttpStatus.SC_OK);
+        //@formatter:on
+    }
 
+    //-------------- test with auth -----------------------
+    @Test
+    public void testCreateAPost_withoutUserAuth_shouldReturn401() throws Exception {
+        PostForm _data = PostForm.builder().title(test_title).content(test_content).build();
+
+        //@formatter:off
+        given()
+            //.auth().basic("user", "password")
+            .body(_data)
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/posts")
+        .then()
+            .statusCode(HttpStatus.SC_UNAUTHORIZED);
+        //@formatter:on
+    }
+
+    @Test
+    public void testCreateAPost_withUserAuth_shouldBeOK() throws Exception {
+        PostForm _data = PostForm.builder().title(test_title).content(test_content).build();
+
+        //@formatter:off
+        given()
+            .auth().basic("user", "password")
+            .body(_data)
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/posts")
+        .then()
+            .header("Location", containsString("/posts"))
+            .statusCode(HttpStatus.SC_CREATED);
+        //@formatter:on
+    }
+
+
+    @Test
+    public void testUpdateAPost_withoutUserAuth_shouldReturn401() throws Exception {
+        PostForm _data = PostForm.builder().title(test_title).content(test_content).build();
+
+        //@formatter:off
+        given()
+            //.auth().basic("user", "password")
+            .body(_data)
+            .contentType(ContentType.JSON)
+        .when()
+            .put("/posts/"+ this.slug)
+        .then()
+            .statusCode(HttpStatus.SC_UNAUTHORIZED);
+        //@formatter:on
+    }
+
+    @Test
+    public void testUpdateAPost_withUserAuth_shouldBeOK() throws Exception {
+        PostForm _data = PostForm.builder().title(test_title).content(test_content).build();
+
+        //@formatter:off
+        given()
+            .auth().basic("user", "password")
+            .body(_data)
+            .contentType(ContentType.JSON)
+        .when()
+            .put("/posts/"+ this.slug)
+        .then()
+            .statusCode(HttpStatus.SC_NO_CONTENT);
+        //@formatter:on
+    }
+
+
+    @Test
+    public void testDeleteAPost_withoutAuth_shouldReturn401() throws Exception {
+
+        //@formatter:off
+        when()
+            .delete("/posts/"+ this.slug)
+        .then()
+            .statusCode(HttpStatus.SC_UNAUTHORIZED);
+        //@formatter:on
+    }
+
+    @Test
+    public void testDeleteAPost_withUserAuth_shouldReturn403() throws Exception {
+
+        //@formatter:off
+        given()
+            .auth().basic("user", "password")
+        .when()
+            .delete("/posts/"+ this.slug)
+        .then()
+            .statusCode(HttpStatus.SC_FORBIDDEN);
+        //@formatter:on
+    }
+
+
+    @Test
+    public void testDeleteAPost_withAdminAuth_shouldOK() throws Exception {
+
+        //@formatter:off
+        given()
+            .auth().basic("admin", "password")
+        .when()
+            .delete("/posts/"+ this.slug)
+        .then()
+            .statusCode(HttpStatus.SC_NO_CONTENT);
+        //@formatter:on
+    }
+
+
+    @Test
+    public void testCreateACommentsOfPostBySlug_withoutAuth_shouldReturn401() throws Exception {
+        CommentForm _data = CommentForm.builder().content(test_comment).build();
+
+        //@formatter:off
+        given()
+            .body(_data)
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/posts/"+ this.slug+"/comments")
+        .then()
+            .statusCode(HttpStatus.SC_UNAUTHORIZED);
+        //@formatter:on
+    }
+
+    @Test
+    public void testCreateACommentsOfPostBySlug_withUserAuth_shouldBeOk() throws Exception {
+        CommentForm _data = CommentForm.builder().content(test_comment).build();
+
+        //@formatter:off
+        given()
+            .auth().basic("user", "password")
+            .body(_data)
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/posts/"+ this.slug+"/comments")
+        .then()
+            .header("Location", containsString("/posts/"+ this.slug+"/comments"))
+            .statusCode(HttpStatus.SC_CREATED);
+        //@formatter:on
+    }
+
+
+    @TestConfiguration
+    @Slf4j
+    static class TestSecurityConfig {
+
+        @Inject
+        PasswordEncoder passwordEncoder;
+
+        @Bean
+        UserDetailsService userDetailsService() {
+            UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+
+            UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+
+            log.debug("dummy user:" + user);
+            log.debug("dummy admin:" + admin);
+
+            return (username) -> {
+                if ("user".equals(username)) {
+                    return user;
+                } else {
+                    return admin;
+                }
+            };
+        }
+    }
 }
